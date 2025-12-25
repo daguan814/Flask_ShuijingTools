a3b951d719610c995d21af2e4ebc1c22d46f1d74
shuijing <lu873737753@163.com>
2025-12-25 18:06:40 +0800

    v2.3 bugfix 修复反向代理

--- app.py
+++ app.py
@@ -13,20 +13,8 @@ service_dir = os.path.join(current_dir, 'Service')
 if service_dir not in sys.path:
     sys.path.insert(0, service_dir)
 
-# 创建Flask应用，使用默认的静态文件URL前缀'/static'
+# 创建Flask应用，使用默认配置
 app = Flask(__name__)
-
-# 添加before_request中间件，处理Nginx代理的路径
-@app.before_request
-def before_request():
-    from flask import request
-    # 检查是否通过Nginx代理访问
-    if request.headers.get('X-Forwarded-For'):
-        # 如果请求路径不是以/text开头，添加/text前缀
-        if not request.path.startswith('/text'):
-            # 重定向到带/text前缀的路径
-            from flask import redirect, url_for
-            return redirect(url_for('text.index'))
 
 # 注册蓝图，使用根路径作为前缀
 app.register_blueprint(text_bp, url_prefix='/')
