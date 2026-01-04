21f323d1ecb41027ff030c1390ea3c066e2065a2
shuijing <lu873737753@163.com>
2026-01-04 16:15:21 +0800

    v2.7 使用清华源加快部署下载依赖的速度

--- run.sh
+++ run.sh
@@ -33,7 +33,7 @@ echo ">>> 当前 conda 环境: $CONDA_DEFAULT_ENV"
 
 # 5️⃣ 安装 / 更新依赖（实时日志）
 echo ">>> 安装 / 更新 Python 依赖（实时日志）"
-pip install --no-cache-dir -r requirements.txt
+pip install --no-cache-dir -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 # 6️⃣ 启动 Flask（前台 + 日志直出）
 echo ">>> 启动 Flask 服务（日志实时输出）"
